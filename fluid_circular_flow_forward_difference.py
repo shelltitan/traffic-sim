fixed numerical approach

fluid_circular_flow_forward_difference.py
@@ -18,11 +18,9 @@ for i in range(size):
     else:
         road_t.append(15)
         road_dt.append(15)
-while time_elapsed < 10000:
+while time_elapsed < 1000:
     for i in range(size):
-        if i == 0:
-            road_dt[i] += 0.01/2 * (v(road_t[i]) * (road_t[i] - road_t[i+1]) + road_t[i] * (v(road_t[i]) - v(road_t[i+1])))
-        elif i == size - 1:
+        if i == size - 1:
             road_dt[i] += 0.01/2 * (v(road_t[i]) * (road_t[i] - road_t[0]) + road_t[i] * (v(road_t[i]) - v(road_t[0])))
         else:
             road_dt[i] += 0.01/2 * (v(road_t[i]) * (road_t[i] - road_t[i+1]) + road_t[i] * (v(road_t[i]) - v(road_t[i+1])))
@@ -47,7 +45,7 @@ array2 = []
 #pic_array = np.array(array2)
 #img = Image.fromarray(pic_array)
 x = range(0, 100)
-y = range(0, 10000)
+y = range(0, 1000)
 X, Y = np.meshgrid(x, y)
 Z = np.array(road_ev)
 fig = plt.figure()
@@ -59,4 +57,3 @@ ax.set_ylabel('y')
 ax.set_zlabel('z')
 ax.view_init(90, 0)
 ax.view_init(60, 30)
-

fluid_circular_flow_forward_difference_type2.py
@@ -9,11 +9,6 @@ time_elapsed = 0
 v_max = 5
 max_density = 30
 
-def v(density):
-    return (1-density/max_density)*v_max
-
-bc_rho = 0
-
 for i in range(size):
     if i < size/2:
         road_t.append(30)
@@ -21,12 +16,14 @@ for i in range(size):
     else:
         road_t.append(15)
         road_dt.append(15)
-while time_elapsed < 1000:
+while time_elapsed < 10000:
     for i in range(size):
-        if i == size - 1:
-            road_dt[i] += 0.01/2 * (v(road_t[i]) * (road_t[i] - bc_rho) + road_t[i] * (v(road_t[i]) - v(bc_rho)))
+        if i == 1:
+            road_dt[i] += 0.01/2 * v_max * (road_t[i] - road_t[i+1] - 2 * (road_t[i] / max_density) * (road_t[i] - road_t[i+1]))
+        elif i == size - 1:
+            road_dt[i] += 0.01/2 * v_max * (road_t[i] - road_t[0] - 2 * (road_t[i] / max_density) * (road_t[i] - road_t[0]))
         else:
-            road_dt[i] += 0.01/2 * (v(road_t[i]) * (road_t[i] - road_t[i+1]) + road_t[i] * (v(road_t[i]) - v(road_t[i+1])))
+            road_dt[i] += 0.01/2 * v_max * (road_t[i] - road_t[i+1] - 2 * (road_t[i] / max_density) * (road_t[i] - road_t[i+1]))
             
     time_elapsed += 1
     road_t = road_dt.copy()
@@ -48,11 +45,15 @@ array2 = []
 #pic_array = np.array(array2)
 #img = Image.fromarray(pic_array)
 x = range(0, 100)
-for i in range(100):
-    if i % 1 == 0:
-        y = np.array(road_ev[i])
-        plt.plot(x,y)
-        plt.xlabel('Position')
-        plt.ylabel('Density')
-        plt.savefig('plot' + str(i) + '.png')
-        plt.clf()
+y = range(0, 10000)
+X, Y = np.meshgrid(x, y)
+Z = np.array(road_ev)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                cmap='viridis', edgecolor='none')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+ax.view_init(90, 0)
+ax.view_init(60, 30)

trafficonelane.py
@@ -20,12 +20,12 @@ error = 0
 lane = []
 picture_lane = []
 for i in range(tile_number):
-    if np.random.uniform() > 0.5:
+    if 500 > i:
         lane.append(-1)
     else:
         lane.append(np.random.randint(0,v_max))
         cars += 1
-while time_elapsed < 10000:
+while time_elapsed < 2000:
     if lane[0] == -1 and 0.5 > np.random.uniform():
         lane[0] = 1
         cars += 1